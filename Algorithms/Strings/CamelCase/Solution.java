9b4480f43777ef71f0f84c9f0d2fdb294da9a56c
Emre Bolat <emre.bolat@gmail.com>
2017-03-01 11:54:59 +0300

    CamelCase

---
+++ Algorithms/Strings/CamelCase/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        int upperCase = 0;
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        
+        for(int i=0; i< s.length();i++){
+           if(Character.isUpperCase(s.charAt(i))){
+               upperCase++;
+           } 
+        }
+        
+        System.out.print(upperCase + 1);
+        scan.close();
+    }
+}
