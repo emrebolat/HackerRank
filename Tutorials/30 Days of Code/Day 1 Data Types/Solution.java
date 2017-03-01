491ae224fbfc65bcec9d3a46fc08b5160bb9c1a0
Emre Bolat <emre.bolat@gmail.com>
2017-03-01 15:48:02 +0300

    30 Days of Code -- Day 0 - 5 Solutions

---
+++ Tutorials/30 Days of Code/Day 1 Data Types/Solution.java
@@ -0,0 +1,12 @@
+        int integerValue;
+        double doubleValue;
+        String stringValue;
+       
+        integerValue = scan.nextInt();
+        doubleValue = scan.nextDouble();
+        scan.nextLine();
+        stringValue = scan.nextLine();
+
+        System.out.println(integerValue + i);
+		System.out.println(doubleValue + d);
+        System.out.println(s + stringValue);
---
+++ Tutorials/30 Days of Code/Day 2 Operators/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.math.*;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble();
+        int tipPercent = scan.nextInt();
+        int taxPercent = scan.nextInt();
+        scan.close();
+      
+        double tip = mealCost * (tipPercent / 100.0);
+        double tax = mealCost * (taxPercent / 100.0);
+       
+        double total = mealCost + tax + tip;
+        int totalCost = (int) Math.round(total);
+        
+        System.out.print("The total meal cost is " + totalCost + " dollars.");
+    }
+}
